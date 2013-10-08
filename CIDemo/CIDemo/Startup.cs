e661d5a95c2b803592e92d508b0e6f33ff6fd381
Iris Classon <iris@irisclasson.com>
2013-10-08 09:42:45 +0200

    First commit

---
+++ CIDemo/CIDemo/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace CIDemo
+{
+    public partial class Startup 
+    {
+        public void Configuration(IAppBuilder app) 
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
