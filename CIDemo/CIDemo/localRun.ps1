cb75a7691f194a1b4d5499dc9a01e4978988fb2a
Iris Classon <iris@irisclasson.com>
2013-10-08 15:51:28 +0200

    PSfiles

---
+++ CIDemo/CIDemo/localRun.ps1
@@ -0,0 +1,5 @@
+﻿$proj = ".\CIDemo.csproj"
+$azure = ".\Properties\PublishProfiles\prof.pubxml"
+$pwd = "HAdm9v74poqq83Jqemhgsqogxn9541myX93GQxpJJlL18CETkB6YwX8obtGl"
+
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe $proj /p:DeployOnBuild=true /p:PublishProfile=Production /p:PublishProfile=$azure /p:VisualStudioVersion=12.0 /p:Password=$pwd /p:AllowUntrustedCertificate=true
