38818af82170400929d50d8e4216de3451708e5a
Iris Classon <iris@irisclasson.com>
2013-10-11 14:44:08 +0200

    test

---
+++ CIDemo/CIDemo/obj/Release/Package/PackageTmp/evry.ps1
@@ -0,0 +1,5 @@
+﻿$proj = ".\CIDemo.csproj"
+$azure = ".\Properties\PublishProfiles\evry.pubxml"
+$pwd = "KundPub2013#"
+
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe $proj /p:DeployOnBuild=true /p:PublishProfile=Production /p:PublishProfile=$azure /p:VisualStudioVersion=12.0 /p:Password=$pwd /p:AllowUntrustedCertificate=true
